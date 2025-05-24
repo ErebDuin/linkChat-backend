de2f7bd435d641bc9558a095defffb6d07221ae5
gegam <erebduin@gmail.com>
2025-05-23 17:47:23 -0700

    Updated main

---
+++ src/main/java/com/practiceproject/linkchat_back/LinkchatBackApplication.java
@@ -0,0 +1,13 @@
+package com.practiceproject.linkchat_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkchatBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkchatBackApplication.class, args);
+	}
+
+}
