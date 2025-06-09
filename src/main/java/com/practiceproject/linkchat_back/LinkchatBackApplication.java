1bf6b56e5c2dfe52fa5c227cb901d2c81465cb6e
Artem Oganesyan <artem.oganesyan@gmail.com>
2025-06-09 15:32:41 -0700

    upd

--- src/main/java/com/practiceproject/linkchat_back/LinkchatBackApplication.java
+++ src/main/java/com/practiceproject/linkchat_back/LinkchatBackApplication.java
@@ -11,23 +11,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class LinkchatBackApplication {
 
-	@Bean
-	CommandLineRunner init(UserRepository repo) {
-		return args -> {
-			// Initialize the database with a default user
-			if (repo.findByUsername("admin") == null) {
-				User admin = new User();
-				admin.setUsername("admin");
-				admin.setPassword("test");
-				repo.save(admin);
-			}
-		};
-	}
+//	@Bean
+//	CommandLineRunner init(UserRepository repo) {
+//		return args -> {
+//			// Initialize the database with a default user
+//			if (repo.findByUsername("admin") == null) {
+//				User admin = new User();
+//				admin.setUsername("admin");
+//				admin.setPassword("test");
+//				repo.save(admin);
+//			}
+//		};
+//	}
 
 	public static void main(String[] args) {
-
-
-
 		SpringApplication.run(LinkchatBackApplication.class, args);
 	}
 }
