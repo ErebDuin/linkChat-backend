07cb86faa56a70eadc820648650eba9e194b6849
Artem Oganesyan <artem.oganesyan@gmail.com>
2025-06-20 17:03:48 -0700

    html email

--- src/main/java/com/practiceproject/linkchat_back/dtos/EmailRequest.java
+++ src/main/java/com/practiceproject/linkchat_back/dtos/EmailRequest.java
@@ -1,9 +1,12 @@
 package com.practiceproject.linkchat_back.dtos;
+import java.util.Map;
 
 public class EmailRequest {
     private String to;
     private String subject;
     private String text;
+    private String templateName;
+    private Map<String, String> templateVariables;
 
     public String getTo() { return to; }
     public void setTo(String to) { this.to = to; }
@@ -11,4 +14,20 @@ public class EmailRequest {
     public void setSubject(String subject) { this.subject = subject; }
     public String getText() { return text; }
     public void setText(String text) { this.text = text; }
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+
+    public Map<String, String> getTemplateVariables() {
+        return templateVariables;
+    }
+
+    public void setTemplateVariables(Map<String, String> templateVariables) {
+        this.templateVariables = templateVariables;
+    }
 }
