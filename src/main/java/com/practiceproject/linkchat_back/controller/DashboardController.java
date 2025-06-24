6a124879e4cd2f1203b720956e36218c4a84e61f
Artem Oganesyan <artem.oganesyan@gmail.com>
2025-06-23 17:04:56 -0700

    MVC example

--- src/main/java/com/practiceproject/linkchat_back/controller/DashboardController.java
+++ src/main/java/com/practiceproject/linkchat_back/controller/DashboardController.java
@@ -1,13 +1,25 @@
 package com.practiceproject.linkchat_back.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DashboardController {
 
+    private String message = "";
+
     @GetMapping("/ui/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+        model.addAttribute("message", message);
         return "dashboard";
+    }
+
+    @PostMapping("/ui/dashboard")
+    public String dashboard(@RequestParam String newMessage) {
+        this.message = newMessage;
+        return "redirect:/ui/dashboard";
     }
 }
