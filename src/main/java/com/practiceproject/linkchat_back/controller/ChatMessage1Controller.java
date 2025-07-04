19f263766238f8bfb93480633378f04e5c0b9ad0
Eugen2712 <eugeniu.plotnic@gmail.com>
2025-07-04 18:41:07 -0500

    Add Chat Message practice v2
    # Conflicts:
    #	src/main/resources/templates/dashboard.html

--- src/main/java/com/practiceproject/linkchat_back/controller/ChatMessage1Controller.java
+++ src/main/java/com/practiceproject/linkchat_back/controller/ChatMessage1Controller.java
@@ -1,12 +1,16 @@
 package com.practiceproject.linkchat_back.controller;
 import com.practiceproject.linkchat_back.model.ChatMessage1;
 import com.practiceproject.linkchat_back.services.ChatMessage1Service;
+import com.practiceproject.linkchat_back.viewModels.ChatMessage1ViewModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ChatMessage1Controller {
@@ -20,12 +24,19 @@ public class ChatMessage1Controller {
     @GetMapping("/ui/chatMessages1")
     public String chatMessages1(Model model) {
         model.addAttribute("chatMessages", service.findAll());
-        model.addAttribute("chatMessageForm", new ChatMessage1());
+        model.addAttribute("chatMessageForm", new ChatMessage1ViewModel());
         return "chatMessages1";
     }
 
     @PostMapping("/ui/chatMessages1/add")
-    public String addChatMessage(@ModelAttribute ChatMessage1 chatMessage) {
+    public String addChatMessage(@Valid @ModelAttribute("chatMessageForm") ChatMessage1ViewModel chatMessageViewModel,
+                                BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("chatMessages", service.findAll());
+            return "chatMessages1";
+        }
+
+        ChatMessage1 chatMessage = convertToEntity(chatMessageViewModel);
         service.save(chatMessage);
         return "redirect:/ui/chatMessages1";
     }
@@ -37,10 +48,24 @@ public class ChatMessage1Controller {
     }
 
     @PostMapping("/ui/chatMessages1/update")
-    public String updateChatMessage(@ModelAttribute ChatMessage1 chatMessage) {
+    public String updateChatMessage(@Valid @ModelAttribute("chatMessageForm") ChatMessage1ViewModel chatMessageViewModel,
+                                  BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("chatMessages", service.findAll());
+            return "chatMessages1";
+        }
+
+        ChatMessage1 chatMessage = convertToEntity(chatMessageViewModel);
         service.save(chatMessage);
         return "redirect:/ui/chatMessages1";
     }
 
-
+    private ChatMessage1 convertToEntity(ChatMessage1ViewModel viewModel) {
+        ChatMessage1 entity = new ChatMessage1();
+        entity.setMessage_id(viewModel.getMessage_id());
+        entity.setRecipient(viewModel.getRecipient());
+        entity.setSender(viewModel.getSender());
+        entity.setTimestamp(viewModel.getTimestamp());
+        return entity;
+    }
 }
