345cad85c6d20dd6dd0ca5a690a042ec3cbc3a59
b0mjur <vitalii.k.usa@gmail.com>
2025-07-13 12:52:31 -0700

    updated send api message with response to client and additional validation

--- src/main/java/com/practiceproject/linkchat_back/dtos/MessageDto.java
+++ src/main/java/com/practiceproject/linkchat_back/dtos/MessageDto.java
@@ -1,8 +1,14 @@
 package com.practiceproject.linkchat_back.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class MessageDto {
-    public String messageText;
-    public String sender;
-    public String recipient;
-    public String timestamp;
+
+    @NotBlank  public String messageText;
+    @NotBlank  public String sender;
+    @NotBlank  public String recipient;
+    @NotNull public String timestamp;
+
+    public record MessageResponse(String message) {}
 }
